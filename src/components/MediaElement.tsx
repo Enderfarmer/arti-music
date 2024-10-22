6395328f73895dbc28f38ded1a8179e729c8d4c9
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-10-22 20:22:37 +0200

    Add single media displaying component

---
+++ src/components/MediaElement.tsx
@@ -0,0 +1,187 @@
+"use client";
+import { MusicTrack } from "@/types";
+import { extname } from "path";
+import { useRef, useEffect, useState } from "react";
+import Image from "next/image";
+
+const audio_formats = [
+    // Audioformate
+    "aac",
+    "ac3",
+    "aif",
+    "aifc",
+    "aiff",
+    "amr",
+    "au",
+    "caf",
+    "flac",
+    "m4a",
+    "m4b",
+    "mp3",
+    "oga",
+    "voc",
+    "wav",
+    "weba",
+    "wma",
+];
+const video_formats = [
+    // Videoformate
+    "3g2",
+    "3gp",
+    "3gpp",
+    "avi",
+    "cavs",
+    "dv",
+    "dvr",
+    "flv",
+    "m2ts",
+    "m4v",
+    "mkv",
+    "mod",
+    "mov",
+    "mp4",
+    "mpeg",
+    "mpg",
+    "mts",
+    "mxf",
+    "ogg",
+    "rm",
+    "rmvb",
+    "swf",
+    "vob",
+    "webm",
+    "wmv",
+    "wtv",
+];
+
+const MediaElement = ({ track }: { track: MusicTrack }) => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const [operation, setOperation] = useState<null | Function>(null);
+    const [mediaElement, setMediaElement] = useState<
+        HTMLAudioElement | HTMLVideoElement | null
+    >(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        // Set the correct media element after the component mounts
+        if (audioRef.current) {
+            setMediaElement(audioRef.current);
+        } else if (videoRef.current) {
+            setMediaElement(videoRef.current);
+        }
+    }, []); // The effect runs once after the initial render
+
+    useEffect(() => {
+        if (mediaElement) {
+            console.log("mediaElement is true");
+            if (mediaElement.readyState >= 1) {
+                setIsLoaded(true);
+            }
+        }
+    }, [mediaElement]);
+
+    // Handle the loaded metadata to set the media element as ready
+    const handleLoadedMetadata = () => {
+        setIsLoaded(true);
+    };
+
+    // Handle the plus button click to move the currentTime forward
+    const handlePlusButtonClick = (e: React.MouseEvent) => {
+        console.log(isLoaded);
+        if (mediaElement && isLoaded) {
+            setOperation(function () {
+                mediaElement.currentTime += 10;
+            });
+        }
+    };
+    const handlePlayButtonClick = (e: React.MouseEvent) => {
+        if (mediaElement) {
+            if (mediaElement.paused) {
+                mediaElement.play();
+                e.currentTarget.setAttribute("src", "/pause.svg");
+                e.currentTarget.setAttribute("alt", "Pause");
+            } else {
+                e.currentTarget.setAttribute("src", "/play.svg");
+                e.currentTarget.setAttribute("alt", "play");
+                mediaElement.pause();
+            }
+        }
+    };
+    const handleMinusButtonClick = (e: React.MouseEvent) => {
+        if (mediaElement) {
+            setOperation(function () {
+                mediaElement.currentTime -= 10;
+            });
+        }
+    };
+    const element = audio_formats.includes(
+        extname(track.music_file).slice(1)
+    ) ? (
+        <>
+            <audio
+                src={track.music_file}
+                controls
+                hidden
+                ref={audioRef}
+                onLoadedMetadata={handleLoadedMetadata}
+            ></audio>
+            <img src={track.image} alt="Track image" height={200} />
+        </>
+    ) : video_formats.includes(extname(track.music_file).slice(1)) ? (
+        <video
+            src={track.music_file}
+            poster={track.image}
+            ref={videoRef}
+            onLoadedMetadata={handleLoadedMetadata}
+        ></video>
+    ) : (
+        "Unknown media format"
+    );
+    if (mediaElement) {
+        mediaElement.onended = function () {
+            document
+                .getElementById("playMedia")
+                ?.setAttribute("src", "/play.svg");
+        };
+        mediaElement.oncanplay = () => {
+            if (typeof operation == "function") operation();
+            setOperation(null);
+        };
+    }
+    return (
+        <div>
+            {element}
+            <div id="playBar">
+                <Image
+                    src={"/-10_seconds.svg"}
+                    alt="Ten seconds back"
+                    width={30}
+                    height={30}
+                    className="inline"
+                    onClick={handleMinusButtonClick}
+                    title="Ten seconds back"
+                />
+                <Image
+                    src="/play.svg"
+                    alt="Play media"
+                    onClick={handlePlayButtonClick}
+                    id="playMedia"
+                    width={30}
+                    height={30}
+                    className="inline"
+                />
+                <Image
+                    src={"/+10_seconds.svg"}
+                    alt="Skip ten seconds"
+                    width={30}
+                    height={30}
+                    className="inline"
+                    onClick={handlePlusButtonClick}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default MediaElement;
