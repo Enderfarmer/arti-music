84bb70fd226d22f8313f51feb5a7e1bec7fc26a5
Enderfarmer <artem.kort@stiftsgymnasium.de>
2024-10-23 19:35:10 +0200

    Adjust the theme for the top navbar to have contrast

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -2,18 +2,31 @@ import Image from "next/image";
 
 const TopNavBar = ({}) => {
     return (
-        <nav>
-            <span className="brand">
-                <span className="text-indigo-700">Arti's</span> Music
+        <nav
+            className="flex bg-gray-900"
+            style={{ justifyContent: "space-between" }}
+        >
+            <span className="brand font-bold m-4 p-1 text-xl">
+                <span className="text-indigo-700 font-extrabold mr-1">
+                    Arti's
+                </span>{" "}
+                Music
             </span>
             <span>
                 <Image
                     src={"/search.svg"}
                     alt={"Search"}
-                    width="50"
-                    height="50"
+                    width="30"
+                    height="30"
+                    className="inline m-3"
                 />
-                <Image src={"/user-icon.svg"} alt="" />
+                <Image
+                    src={"/user-icon.svg"}
+                    alt=""
+                    width={30}
+                    height={30}
+                    className="inline m-4 ml-2"
+                />
             </span>
         </nav>
     );
